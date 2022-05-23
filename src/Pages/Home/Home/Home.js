f1acb306c5126b3a71d280e241b3a1eb905fa13f
tanverrana <tanversobur@gmail.com>
2022-05-24 00:46:07 +0600

    about us added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import About from '../About/About';
 import Banner from '../Banner/Banner';
 import BusinessSummary from '../BusinessSummary/BusinessSummary';
+import Partners from '../Partners/Partners';
 
 const Home = () => {
     return (
         <div className="mx-20">
             <Banner></Banner>
             <BusinessSummary></BusinessSummary>
+            <About></About>
         </div>
     );
 };
