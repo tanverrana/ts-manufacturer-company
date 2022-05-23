69ea2981e4c713b0be20b0dc187fe661578d0b31
tanverrana <tanversobur@gmail.com>
2022-05-23 23:56:13 +0600

    add business summary

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import BusinessSummary from '../BusinessSummary/BusinessSummary';
 
 const Home = () => {
     return (
         <div className="mx-20">
             <Banner></Banner>
+            <BusinessSummary></BusinessSummary>
         </div>
     );
 };
