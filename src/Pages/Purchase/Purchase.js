6d3084b2cee4c88935298baf9e3d1450b02da936
tanverrana <tanversobur@gmail.com>
2022-05-24 23:05:37 +0600

    get single product

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
     const { toolsId } = useParams();
+    const [tool, setTool] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/tool/${toolsId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setTool(data))
+    }, [])
     return (
         <div>
             <h2>Welcome To purchase page:{toolsId}</h2>
+            <h1>Name:{tool.name}</h1>
         </div>
     );
 };
