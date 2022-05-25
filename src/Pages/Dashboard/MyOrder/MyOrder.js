d6594bd170e419679fda0f60adafc1ca63949f58
tanverrana <tanversobur@gmail.com>
2022-05-26 00:15:14 +0600

    create nested route and my order page

--- src/Pages/Dashboard/MyOrder/MyOrder.js
+++ src/Pages/Dashboard/MyOrder/MyOrder.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyOrder = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?customer=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data));
+        }
+    }, [user])
     return (
         <div>
-            <h2>My Order</h2>
+            <h2 className="text-2xl font-bold">My Order:{orders.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Tool Name</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{order.customerName}</td>
+                                <td>{order.toolsName}</td>
+                                <td>{order.quantity}</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
