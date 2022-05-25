08ae438b75aeb135553224aedf7522b7af96529e
tanverrana <tanversobur@gmail.com>
2022-05-25 18:04:27 +0600

    sign in with google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className="flex h-screen justify-center items-center">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-2xl text-center font-bold">Please Login!</h2>
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline">Sign In With Google</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyD9QLVizoHjSMPGVWgJV001WAcS0VnHH_c",
+    authDomain: "ts-manufacture-company.firebaseapp.com",
+    projectId: "ts-manufacture-company",
+    storageBucket: "ts-manufacture-company.appspot.com",
+    messagingSenderId: "1091174219268",
+    appId: "1:1091174219268:web:f494dc9243ac4da5ed7f51"
 };
 
 // Initialize Firebase
