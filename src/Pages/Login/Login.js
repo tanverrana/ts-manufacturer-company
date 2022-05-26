ca6596a1b54ab200dcdacfb7fa35dd367e6ec9d4
tanverrana <tanversobur@gmail.com>
2022-05-26 08:08:11 +0600

    create token and conncet login and signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,8 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
+import { useEffect } from 'react';
 
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -15,6 +17,8 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    const [token] = useToken(user || gUser)
+
     let signInErrorMessage;
 
     const navigate = useNavigate();
@@ -22,15 +26,21 @@ const Login = () => {
 
     let from = location.state?.from?.pathname || "/";
 
+    useEffect(() => {
+        if (token) {
+            navigate(from, { replace: true });
+        }
+    }, [token, from, navigate])
+
     if (loading || gLoading) {
         return <Loading></Loading>
     }
     if (error || gError) {
         signInErrorMessage = <p className="text-red-500">{error?.message || gError?.message}</p>
     }
-    if (gUser || user) {
+    /* if (gUser || user) {
         navigate(from, { replace: true });
-    }
+    } */
 
     const onSubmit = data => {
         console.log(data);
