4874a71c44f049f372d1e4af324b57d07b5d3060
tanverrana <tanversobur@gmail.com>
2022-05-27 02:03:46 +0600

    my profile added

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+
     return (
         <div>
-            <h2>My Profile</h2>
+            <h2 className="text-3xl font-bold text-orange-500">My Profile</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-6/12">
+
+                    <thead>
+                        <tr>
+
+                            <th>Profile Name</th>
+                            <th>Email</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+
+                            <td>{user.displayName}</td>
+                            <td>{user.email}</td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
