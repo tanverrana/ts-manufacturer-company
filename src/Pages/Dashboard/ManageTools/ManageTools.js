1a13684595fb830bbb7293781192d14cdffa374b
tanverrana <tanversobur@gmail.com>
2022-05-27 10:35:01 +0600

    load all tools in managetools page

--- src/Pages/Dashboard/ManageTools/ManageTools.js
+++ src/Pages/Dashboard/ManageTools/ManageTools.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useTools from '../../../hooks/useTools';
+import ManageTool from './ManageTool';
 
 const ManageTools = () => {
+    const [tools] = useTools();
     return (
         <div>
             <h2>Manage Tools</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid:cols-3 gap-10">
+                {
+                    tools.map(tool => <ManageTool
+                        key={tool._id}
+                        tool={tool}
+                    ></ManageTool>)
+                }
+            </div>
         </div>
     );
 };
