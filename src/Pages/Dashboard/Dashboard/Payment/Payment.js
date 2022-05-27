a93ea2cba380b7b90a8b8689ae9cb6f636f9ee1b
tanverrana <tanversobur@gmail.com>
2022-05-27 16:46:02 +0600

    get card information

--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../../../Shared/Loading';
+import CheckoutForm from '../../CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L1klHEgDuzWA1sXCedxIaEbehywdv0aIEGPIhWs2icIqoF87iqwRiTpZzg5TI5TJsjbI6OXWhkihpOKoMXX1A4N00Vam9nhZQ');
+
 
 const Payment = () => {
     const { id } = useParams();
@@ -28,7 +34,9 @@ const Payment = () => {
                 </div>
                 <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                     <div class="card-body">
-
+                        <Elements stripe={stripePromise}>
+                            <CheckoutForm />
+                        </Elements>
 
                     </div>
                 </div>
