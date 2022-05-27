318bbe947164c5580916838a130e6c1ba7f7119c
tanverrana <tanversobur@gmail.com>
2022-05-27 14:37:05 +0600

    manage order page data load

--- src/Pages/Dashboard/ManageOrders/ManageOrders.js
+++ src/Pages/Dashboard/ManageOrders/ManageOrders.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageOrders = () => {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/orders")
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, []);
     return (
         <div>
-            <h2>Manage All Orders</h2>
+            <h2>Manage All Orders:{orders.length}</h2>
+            <div>
+
+            </div>
         </div>
     );
 };
